9619609bdd77f473392e895f43cb77204dcd8a4a
Quentin <quentin.haenn@ensma.fr>
2024-04-30 14:03:41 +0200

    Models + firsts UT

--- avocadogrowth/avocadoapi/models.py
+++ avocadogrowth/avocadoapi/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Stacks(models.Model):
+    id = models.AutoField(primary_key=True)
+    tag = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tag
+
+
+class User(AbstractUser):
+    GENDER_CHOICES = {
+        "M": "Male",
+        "F": "Female",
+        "O": "Other",
+    }
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, blank=True, null=True, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    online_status = models.BooleanField(default=False)
+    is_banned = models.BooleanField(default=False)
+    bio = models.TextField(blank=True, null=True)
+    urls = models.JSONField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    mentors = models.ManyToManyField('Mentor', symmetrical=False, default=None, related_name='my_mentors')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    learning_stacks = models.ManyToManyField(Stacks, default=None)
+
+    def __str__(self):
+        return self.username if self.username else self.email
+
+
+class Mentor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    description = models.TextField()
+    rating = models.FloatField(null=True, blank=True)
+    is_available = models.BooleanField(default=False)
+    history = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    stacks = models.ManyToManyField(Stacks)
+
+    def __str__(self):
+        return self.user.username if self.user.username else self.user.email
+
+
+class Comments(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment = models.TextField()
+    rating = models.FloatField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_commenting')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_commented')
+    stacks = models.ManyToManyField(Stacks)
+
+    def __str__(self):
+        return f"Comment by {self.from_user.email} to {self.to_user.email}"
+
+
+class Requests(models.Model):
+    STATUS_CHOICES = {
+        "P": "Pending",
+        "A": "Accepted",
+        "R": "Rejected",
+    }
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_requesting')
+    to_mentor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mentor_requested')
+    stacks = models.ManyToManyField(Stacks)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"Request from {self.from_user.email} to {self.to_mentor.email}"
---
+++ avocadogrowth/avocadoapi/tests/tests_models.py
@@ -0,0 +1,176 @@
+from django.test import TestCase
+
+from avocadoapi.models import User, Mentor, Comments
+
+user_john = {
+    "first_name": "John",
+    "last_name" : "Doe",
+    "email"     : "john@doe.com",
+    "password"  : "password",
+}
+user_jerry = {
+    "first_name": "Jerry",
+    "last_name" : "Doe",
+    "email"     : "jerry@doe.com",
+    "password"  : "password",
+}
+
+
+def setUpUsers():
+    User.objects.create(**user_john)
+    User.objects.create(**user_jerry)
+
+
+def setUpMentors():
+    Mentor.objects.create(user=User.objects.get(email="jerry@doe.com"))
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        setUpUsers()
+
+    def test_new_user(self):
+        new_user = User(first_name="Jane", last_name="Doe", email="jane@doe.com", password="password")
+        self.assertEqual(new_user.first_name, "Jane")
+        self.assertEqual(new_user.last_name, "Doe")
+        self.assertEqual(new_user.email, "jane@doe.com")
+        self.assertEqual(new_user.password, "password")
+        new_user.save()
+        self.assertEqual(new_user.id, 3)
+
+    def test_user_retrieving_with_email(self):
+        john = User.objects.get(email="john@doe.com")
+        self.assertEqual(john.first_name, "John")
+        self.assertEqual(john.last_name, "Doe")
+
+    def test_user_retrieving_with_id(self):
+        john = User.objects.get(id=1)
+        self.assertEqual(john.first_name, "John")
+        self.assertEqual(john.last_name, "Doe")
+
+    def test_user_listing(self):
+        users = User.objects.all()
+        self.assertEqual(len(users), 2)
+        self.assertEqual(users[0].first_name, "John")
+        self.assertEqual(users[1].first_name, "Jerry")
+
+    def test_user_updating(self):
+        john = User.objects.get(email="john@doe.com")
+        john.first_name = "Johnny"
+        john.save()
+        john = User.objects.get(email="john@doe.com")
+        self.assertEqual(john.first_name, "Johnny")
+
+    def test_user_deleting(self):
+        john = User.objects.get(email="john@doe.com")
+        john.delete()
+        self.assertEqual(User.objects.count(), 1)
+        assert not User.objects.filter(email="john@doe.com").exists()
+
+    def test_user_deleting_all(self):
+        User.objects.all().delete()
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_user_deleting_with_id(self):
+        User.objects.filter(id=1).delete()
+        self.assertEqual(User.objects.count(), 1)
+        assert not User.objects.filter(id=1).exists()
+
+    def test_user_deleting_with_email(self):
+        User.objects.filter(email="john@doe.com").delete()
+        self.assertEqual(User.objects.count(), 1)
+        assert not User.objects.filter(email="john@doe.com").exists()
+
+    def test_user_cannot_have_same_email(self):
+        with self.assertRaises(Exception):
+            User.objects.create(**self.user_john)
+
+
+class TestMentorModel(TestCase):
+    def setUp(self):
+        setUpUsers()
+        User.objects.create(first_name="Jane", last_name="Doe", email="jane@doe.com", password="password")
+        setUpMentors()
+
+    def test_new_mentor(self):
+        john = User.objects.get(email="john@doe.com")
+        mentor = Mentor(user=john, description="I am a mentor", rating=4.5, is_available=True, history=10)
+        mentor.save()
+        self.assertEqual(mentor.user.first_name, "John")
+        self.assertEqual(mentor.description, "I am a mentor")
+        self.assertEqual(mentor.rating, 4.5)
+        self.assertEqual(mentor.is_available, True)
+        self.assertEqual(mentor.history, 10)
+
+    def test_mentor_retrieving_with_user(self):
+        jerry = User.objects.get(email="jerry@doe.com")
+        mentor = Mentor.objects.get(user=jerry)
+        self.assertEqual(mentor.user.first_name, "Jerry")
+        self.assertEqual(mentor.description, "")
+        self.assertEqual(mentor.rating, None)
+        self.assertEqual(mentor.is_available, False)
+        self.assertEqual(mentor.history, 0)
+
+    def test_mentor_listing(self):
+        mentors = Mentor.objects.all()
+        self.assertEqual(len(mentors), 1)
+        self.assertEqual(mentors[0].user.first_name, "Jerry")
+
+    def test_mentor_updating(self):
+        jerry = User.objects.get(email="jerry@doe.com")
+        mentor = Mentor.objects.get(user=jerry)
+        mentor.description = "I am a mentor"
+        mentor.save()
+        mentor = Mentor.objects.get(user=jerry)
+        self.assertEqual(mentor.description, "I am a mentor")
+
+    def test_mentor_deleting(self):
+        jerry = User.objects.get(email="jerry@doe.com")
+        mentor = Mentor.objects.get(user=jerry)
+        mentor.delete()
+        self.assertEqual(Mentor.objects.count(), 0)
+        assert not Mentor.objects.filter(user=jerry).exists()
+
+    def test_mentor_deleting_all(self):
+        Mentor.objects.all().delete()
+        self.assertEqual(Mentor.objects.count(), 0)
+
+    def test_add_mentor_to_user(self):
+        john = User.objects.get(email="john@doe.com")
+        jerry = User.objects.get(email="jerry@doe.com")
+        mentor = Mentor.objects.get(user=jerry)
+        john.mentors.add(mentor)
+        john.save()
+        john = User.objects.get(email="john@doe.com")
+        self.assertEqual(john.mentors.count(), 1)
+        self.assertEqual(john.mentors.first().user.first_name, "Jerry")
+
+    def test_remove_mentor_from_user(self):
+        john = User.objects.get(email="john@doe.com")
+        jerry = User.objects.get(email="jerry@doe.com")
+        mentor = Mentor.objects.get(user=jerry)
+        john.mentors.add(mentor)
+        john.save()
+        john = User.objects.get(email="john@doe.com")
+        self.assertEqual(john.mentors.count(), 1)
+        john.mentors.remove(mentor)
+        john.save()
+        john = User.objects.get(email="john@doe.com")
+        self.assertEqual(john.mentors.count(), 0)
+
+
+class TestCommentsModel(TestCase):
+    def setUp(self):
+        setUpUsers()
+        setUpMentors()
+
+    def test_new_comment(self):
+        john = User.objects.get(email="john@doe.com")
+        jerry = User.objects.get(email="jerry@doe.com")
+        mentor_jerry = Mentor.objects.get(user=jerry)
+        comment = Comments(comment="Great mentor", rating=5, from_user=john, to_user=jerry)
+        comment.save()
+
+
+
