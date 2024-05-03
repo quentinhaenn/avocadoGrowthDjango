1291fc3b2a870e9d9db13c478d8afa6eb7ff1bdd
Quentin <quentin.haenn@ensma.fr>
2024-05-03 10:43:32 +0200

    removing classmethod decorators

--- avocadogrowth/avocadoapi/tests/tests_utils.py
+++ avocadogrowth/avocadoapi/tests/tests_utils.py
@@ -4,26 +4,26 @@ from django.test import TestCase
 
 class TestTrie(TestCase):
 
-    def test_setup(self):
+    def test_instantiate(self):
         trie = Trie()
         assert trie is not None
 
     def test_insert(self):
-        trie = Trie()
-        trie.insert("python")
-        assert trie.search("python")
-        assert not trie.search("java")
-        trie.insert("java")
-        assert trie.search("java")
+        trie_insert = Trie()
+        trie_insert.insert("python")
+        assert trie_insert.search("python")
+        assert not trie_insert.search("java")
+        trie_insert.insert("java")
+        assert trie_insert.search("java")
 
     def test_start_with(self):
-        trie = Trie()
-        trie.insert("python")
-        trie.insert("java")
-        trie.insert("javascript")
-        assert trie.starts_with("py")
-        assert trie.starts_with("ja")
-        assert not trie.starts_with("c")
+        trie_start = Trie()
+        trie_start.insert("python")
+        trie_start.insert("java")
+        trie_start.insert("javascript")
+        assert trie_start.starts_with("py")
+        assert trie_start.starts_with("ja")
+        assert not trie_start.starts_with("c")
 
     def test_delete(self):
         trie = Trie()
@@ -70,25 +70,25 @@ class TestTrie(TestCase):
 
     def test_init_from_list(self):
         list_of_words = ["python", "java", "javascript", "c++", "c#"]
-        trie = Trie()
-        trie.initialize(list_of_words)
-        assert trie is not None
-        assert trie.search("python")
-        assert trie.search("java")
-        assert trie.search("javascript")
-        assert trie.search("c++")
-        assert trie.search("c#")
+        trie_list = Trie()
+        trie_list.initialize(list_of_words)
+        assert trie_list is not None
+        assert trie_list.search("python")
+        assert trie_list.search("java")
+        assert trie_list.search("javascript")
+        assert trie_list.search("c++")
+        assert trie_list.search("c#")
 
     def test_autocomplete(self):
-        trie = Trie()
-        trie.insert("python")
-        trie.insert("java")
-        trie.insert("javascript")
-        trie.insert("c++")
-        trie.insert("c#")
-        auto_python = trie.find_words("py")
-        auto_java = trie.find_words("ja")
-        auto_c = trie.find_words("c")
+        trie_auto = Trie()
+        trie_auto.insert("python")
+        trie_auto.insert("java")
+        trie_auto.insert("javascript")
+        trie_auto.insert("c++")
+        trie_auto.insert("c#")
+        auto_python = trie_auto.find_words("py")
+        auto_java = trie_auto.find_words("ja")
+        auto_c = trie_auto.find_words("c")
         assert auto_python == ["python"]
         assert auto_java == ["java", "javascript"]
         assert auto_c == ["c++", "c#"]
--- avocadogrowth/avocadoapi/utils/trie.py
+++ avocadogrowth/avocadoapi/utils/trie.py
@@ -18,14 +18,15 @@ class Trie:
     - initialize(data: Union[str, List[str]], separator: Optional[str] = None) -> None:
     Initializes the trie with a list of words or a string of words.
     """
-    root = TrieNode()
 
-    @classmethod
-    def insert(cls, word):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
         """
         Inserts a word into the trie.
         """
-        node = cls.root
+        node = self.root
         word = word.lower()
         for char in word:
             if char not in node.children:
@@ -33,12 +34,11 @@ class Trie:
             node = node.children[char]
         node.is_end_of_word = True
 
-    @classmethod
-    def search(cls, word):
+    def search(self, word):
         """
         Searches for a word in the trie.
         """
-        node = cls.root
+        node = self.root
         word = word.lower()
         for char in word:
             if char not in node.children:
@@ -46,12 +46,11 @@ class Trie:
             node = node.children[char]
         return node.is_end_of_word
 
-    @classmethod
-    def starts_with(cls, prefix):
+    def starts_with(self, prefix):
         """
         Searches for a prefix in the trie.
         """
-        node = cls.root
+        node = self.root
         prefix = prefix.lower()
         for char in prefix:
             if char not in node.children:
@@ -59,16 +58,14 @@ class Trie:
             node = node.children[char]
         return True
 
-    @classmethod
-    def delete(cls, word):
+    def delete(self, word):
         """
         Deletes a word from the trie.
         """
         word = word.lower()
-        cls._delete_recursive(cls.root, word, 0)
+        self._delete_recursive(self.root, word, 0)
 
-    @classmethod
-    def _delete_recursive(cls, node, word, index):
+    def _delete_recursive(self, node, word, index):
         """
         Helper function for deleting a word from the trie.
         """
@@ -81,48 +78,44 @@ class Trie:
         if char not in node.children:
             return
         child_node = node.children[char]
-        cls._delete_recursive(child_node, word, index + 1)
+        self._delete_recursive(child_node, word, index + 1)
         if child_node.children == {} and not child_node.is_end_of_word:
             del node.children[char]
 
-    @classmethod
-    def _from_str(cls, string, separator=" "):
+    def _from_str(self, string, separator=" "):
         """
         Initializes the trie with a string of words.
         """
         words = string.split(separator)
         for word in words:
-            cls.insert(word)
+            self.insert(word)
 
-    @classmethod
-    def _from_list(cls, words):
+    def _from_list(self, words):
         """
         Initializes the trie with a list of words.
         """
         for word in words:
-            cls.insert(word)
+            self.insert(word)
 
-    @classmethod
-    def initialize(cls, data, separator=None):
+    def initialize(self, data, separator=None):
         """
         Initializes the trie with a list of words or a string of words.
         """
         if isinstance(data, str):
             if not separator:
-                cls._from_str(data)
+                self._from_str(data)
             else:
-                cls._from_str(data, separator)
+                self._from_str(data, separator)
         elif isinstance(data, list):
-            cls._from_list(data)
+            self._from_list(data)
         else:
             raise ValueError("Data type not supported")
 
-    @classmethod
-    def find_words(cls, prefix):
+    def find_words(self, prefix):
         """
         Finds all words with a given prefix.
         """
-        node = cls.root
+        node = self.root
         prefix = prefix.lower()
         words = []
         for char in prefix:
@@ -133,28 +126,26 @@ class Trie:
         if node.is_end_of_word:
             words.append(prefix)
 
-        cls._find_words_recursive(node, prefix, words)
+        self._find_words_recursive(node, prefix, words)
         return words
 
-    @classmethod
-    def _find_words_recursive(cls, node, prefix, words):
+    def _find_words_recursive(self, node, prefix, words):
         """
         Helper function for finding all words with a given prefix.
         """
         if node.is_end_of_word and prefix not in words:
             words.append(prefix)
         for char, child_node in node.children.items():
-            cls._find_words_recursive(child_node, prefix + char, words)
+            self._find_words_recursive(child_node, prefix + char, words)
 
     def __str__(self):
         self._print_recursive(self.root, "")
 
-    @classmethod
-    def _print_recursive(cls, node, prefix):
+    def _print_recursive(self, node, prefix):
         """
         Helper function for printing the trie.
         """
         if node.is_end_of_word:
             print(prefix)
         for char, child_node in node.children.items():
-            cls._print_recursive(child_node, prefix + char)
+            self._print_recursive(child_node, prefix + char)
